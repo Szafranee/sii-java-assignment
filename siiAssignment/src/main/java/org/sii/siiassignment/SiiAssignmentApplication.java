357a4eeb21c5c283da3dbba9a0c5a5808b84c96c
Kacper Szafrański <kacperr.szafranski@gmail.com>
2025-05-11 23:51:10 +0200

    Add initial Spring Boot project files

---
+++ siiAssignment/src/main/java/org/sii/siiassignment/SiiAssignmentApplication.java
@@ -0,0 +1,13 @@
+package org.sii.siiassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiiAssignmentApplication.class, args);
+    }
+
+}
---
+++ siiAssignment/src/test/java/org/sii/siiassignment/SiiAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sii.siiassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiiAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
