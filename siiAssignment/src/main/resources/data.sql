0fabc92b4cf3aa0968c29811505bac4f19d225cf
Kacper Szafrański <kacperr.szafranski@gmail.com>
2025-05-14 01:42:21 +0200

    Refactor comments and formatting in data.sql file
    Removed redundant comments and adjusted table data formatting for better clarity.

--- siiAssignment/src/main/resources/data.sql
+++ siiAssignment/src/main/resources/data.sql
@@ -6,16 +6,14 @@ INSERT INTO FUNDRAISING_EVENT (id, name, account_currency, account_balance) VALU
 ('44444444-4444-4444-4444-444444444444', 'Scholarships for Talented Youth', 'PLN', 0.00);
 
 -- Sample data for the COLLECTION_BOX table
--- fundraising_event_id refers to the ID from the FUNDRAISING_EVENT table
 INSERT INTO COLLECTION_BOX (id, fundraising_event_id) VALUES
-('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', '11111111-1111-1111-1111-111111111111'), -- Box for "Support for Children in Need"
-('bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb', '11111111-1111-1111-1111-111111111111'), -- Second box for "Support for Children in Need"
-('cccccccc-cccc-cccc-cccc-cccccccccccc', '22222222-2222-2222-2222-222222222222'), -- Box for "Saving Homeless Cats"
-('dddddddd-dddd-dddd-dddd-dddddddddddd', '33333333-3333-3333-3333-333333333333'), -- Box for "New Equipment for Local Hospital"
-('eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee', NULL);                                   -- Box not assigned to any fundraising event
+('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', '11111111-1111-1111-1111-111111111111'),
+('bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb', '11111111-1111-1111-1111-111111111111'),
+('cccccccc-cccc-cccc-cccc-cccccccccccc', '22222222-2222-2222-2222-222222222222'),
+('dddddddd-dddd-dddd-dddd-dddddddddddd', '33333333-3333-3333-3333-333333333333'),
+('eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee', NULL);
 
 -- Sample data for the COLLECTION_BOX_AMOUNTS table
--- collection_box_id refers to the ID from the COLLECTION_BOX table
 -- Box 1 (aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa)
 INSERT INTO COLLECTION_BOX_AMOUNTS (collection_box_id, currency, amount) VALUES
 ('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'PLN', 120.50),
@@ -31,7 +29,6 @@ INSERT INTO COLLECTION_BOX_AMOUNTS (collection_box_id, currency, amount) VALUES
 ('cccccccc-cccc-cccc-cccc-cccccccccccc', 'USD', 50.00);
 
 -- Box 4 (dddddddd-dddd-dddd-dddd-dddddddddddd) - empty
--- You can explicitly insert zero, or simply not insert records for empty currencies
 INSERT INTO COLLECTION_BOX_AMOUNTS (collection_box_id, currency, amount) VALUES
 ('dddddddd-dddd-dddd-dddd-dddddddddddd', 'USD', 0.00);
 
